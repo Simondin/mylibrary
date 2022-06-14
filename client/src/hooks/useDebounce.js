aaf0fab5d9fd73deaee02647c8c1a9b97f2d4503
Simone Di Marco <simone.dimarco@cyberdyne.it>
2022-06-14 10:13:10 +0200

    Adds basic SearchBooks component with infinite scrool.
    * SearchPage use infinite loop made with useIntersect custom hook.

---
+++ client/src/hooks/useDebounce.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+
+/**
+ * Custom hook to handle value that have to be debounced.
+ * @param {*} value The value to debounce
+ * @param {*} delay The delay value: the default value is 300ms.
+ * @returns debouncedValue
+ */
+export default function useDebounce(value, delay=300) {
+	const [debouncedValue, setDebouncedValue] = useState(value)
+
+	useEffect(() => {
+		const handler = setTimeout(() => {
+			setDebouncedValue(value)
+		}, delay)
+
+		// Cancel the timeout if value changes (also on delay change or unmount)
+		// This is how we prevent debounced value from updating if value is changed
+		// within the delay period. Timeout gets cleared and restarted.
+		return () => {
+			clearTimeout(handler)
+		}
+	}, [value, delay])
+
+	return debouncedValue
+}
